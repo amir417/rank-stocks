64a2c3d03417662273e38619432761e091ae5f5f
Amirreza <aazam.amirreza@gmail.com>
2022-12-22 19:04:15 -0500

    inital commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Rank Stocks is currently in progress!
+        </p>
+        <p>
+          Author: <a target="_blank" className="personal-link" href='https://www.linkedin.com/in/amirrezaaazam/'>Amirreza Aazam</a>
+        </p>
+        <a
+          className="App-link"
+          href="https://visionary-hotteok-0edc13.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          checkout my personal portfolio in the meantime
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
